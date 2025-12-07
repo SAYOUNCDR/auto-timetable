2b87ca69c0a673d6f8eaa555ab71810367ab639e
0xSYN.dev <pheonixgamer12345@gmail.com>
2025-12-08 03:49:16 +0530

    chore(app.js): config app.js config routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Route files
+const devRoutes = require("./routes/devRoutes");
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.use("/api/dev", devRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Ok server is healthy and running fine" });
 });
+
 module.exports = app;
