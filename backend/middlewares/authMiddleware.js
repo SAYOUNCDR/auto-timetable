49afe719ad8e56cfeb17c52581f1753973c04627
0xSYN.dev <pheonixgamer12345@gmail.com>
2025-12-08 03:09:51 +0530

    feat(auth: added authmiddleware for user and admin

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: "Forbidden: Invalid token" });
+        }
+        req.user = decoded; // {id, role, email}
+        next();
+    });
+};
+
+
+const verifyAdmin = (req, res, next) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({ message: "Forbidden: Admins only" });
+    }
+    next();
+};
+
+module.exports = { verifyToken, verifyAdmin };
